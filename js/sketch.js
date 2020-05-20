3618818bd144e459639b924a7d5d5e328eb94b8d
llaesser <60931177+llaesser@users.noreply.github.com>
2020-05-20 13:31:55 +0200

    end

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,33 @@
-// ----------------- GÉNÉRATEUR --------------------------- //
-
-// une unité = 1 seconde
-let timer = 1;
+let timer = 0.5;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
+    createCanvas(window.innerWidth, window.innerHeight);
+    rectMode(CENTER);
 }
 
 function draw() {
-  // Conditionellle nous permettant de savoir si la souris est appuyée 
-  // et si le timer est écoulé
-  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    generator(width / 2, height / 2, height-150);
-  }
+    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+        generator(width / 2, height / 2, height - 150);
+    }
 }
 
-// Fonction avec quatres paramètres
 function generator(x, y, size) {
-  // fill utilise le paramètre de notre fonction
-  for (let offset = size; offset > 0; offset -= 10) { 
-      fill(random(0,230));
-      rect(x, y, offset, offset);
-      console.log(offset);
-  }
+    for (let offset = size; offset > 0; offset -= 10) {
+        fill(random(255, 100));
+
+
+        noStroke();
+        console.log(offset);
+    }
+    translate(width / 2, height / 2);
+    translate(p5.Vector.fromAngle(millis() / 20, 400));
+    rect(200, 200, 5500, 550);
+    rect(200, 800, 5500, 55);
+    rect(200, 800, 55, 5500);
+    rect(100, 800, 5500, 550);
+
 }
 
-function mousePressed() { 
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-  save();
-}
-
-function windowResized() { 
+function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
